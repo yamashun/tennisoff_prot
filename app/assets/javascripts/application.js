696d50aabeaf2f49b7ce99021b6916eaffece3d0
yamada <yshunske@gmail.com>
2016-05-25 18:25:10 +0900

    検索画面の改善 ・日付項目はテキストに変更 ・date pickerを利用して入力するように変更

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,10 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require turbolinks
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
+//= require jquery-ui
+//= require jquery-ui/datepicker
 //= require bootstrap
 //= require_tree .
