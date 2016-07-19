85fe2a3580e07e3be8cd83046aeb44d36cadf4c7
yamada <yshunske@gmail.com>
2016-07-19 23:56:18 +0900

    検索画面のページネーション追加 turbolinks削除

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,6 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require turbolinks
 //= require underscore
 //= require gmaps/google
 //= require jquery
