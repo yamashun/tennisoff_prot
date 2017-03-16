fb1cb876d916f8aa41530b2d4f0367545a768c76
yamada <yshunske@gmail.com>
2017-03-16 22:15:20 +0900

    add off_meeting_model_test

--- test/models/off_meeting_test.rb
+++ test/models/off_meeting_test.rb
@@ -1,3 +1,22 @@
+# == Schema Information
+#
+# Table name: off_meetings
+#
+#  id                  :integer          not null, primary key
+#  day                 :date
+#  address             :string(255)
+#  level               :string(255)
+#  detail              :text(65535)
+#  created_at          :datetime         not null
+#  updated_at          :datetime         not null
+#  summary             :string(255)
+#  num_of_persons      :integer
+#  user_id             :integer
+#  address_postal_code :string(255)
+#  latitude            :float(24)
+#  longitude           :float(24)
+#
+
 require 'test_helper'
 
 class OffMeetingTest < ActiveSupport::TestCase
