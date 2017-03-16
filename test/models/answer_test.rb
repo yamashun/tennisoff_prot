702c78864f4d31280e3995286190b7d70719c500
yamada <yshunske@gmail.com>
2017-03-16 22:14:35 +0900

    add entry_model_test

--- test/models/answer_test.rb
+++ test/models/answer_test.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: answers
+#
+#  id          :integer          not null, primary key
+#  answer      :text(65535)
+#  user_id     :integer
+#  question_id :integer
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 require 'test_helper'
 
 class AnswerTest < ActiveSupport::TestCase
