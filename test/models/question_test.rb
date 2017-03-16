cd8d3a4f5ef8e11389d7470326f89f88c814cb7d
yamada <yshunske@gmail.com>
2017-03-16 22:16:01 +0900

    question_model_test

--- test/models/question_test.rb
+++ test/models/question_test.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: questions
+#
+#  id             :integer          not null, primary key
+#  question       :text(65535)
+#  user_id        :integer
+#  off_meeting_id :integer
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#
+
 require 'test_helper'
 
 class QuestionTest < ActiveSupport::TestCase
