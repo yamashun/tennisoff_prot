6aef2c27cd1d8aaf9afa93eb253c7b26bd4d4d76
yamada <yshunske@gmail.com>
2017-03-16 22:19:54 +0900

    area_model_test

--- test/models/area_test.rb
+++ test/models/area_test.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: areas
+#
+#  id             :integer          not null, primary key
+#  postal_code    :string(255)      not null
+#  prefectural_id :integer
+#  city           :string(255)      default("")
+#  street         :string(255)      default("")
+#
+
 require 'test_helper'
 
 class AreaTest < ActiveSupport::TestCase
