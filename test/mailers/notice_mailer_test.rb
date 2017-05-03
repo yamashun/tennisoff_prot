69c1109527deac8984c4ed821cd202f70192ca9a
yamada <yshunske@gmail.com>
2017-05-03 14:03:58 +0900

    未使用のテストをコメントアウト

--- test/mailers/notice_mailer_test.rb
+++ test/mailers/notice_mailer_test.rb
@@ -1,12 +1,12 @@
 require 'test_helper'
 
 class NoticeMailerTest < ActionMailer::TestCase
-  test "send_when_update" do
-    mail = NoticeMailer.send_when_update
-    assert_equal "Send when update", mail.subject
-    assert_equal ["to@example.org"], mail.to
-    assert_equal ["from@example.com"], mail.from
-    assert_match "Hi", mail.body.encoded
-  end
+  # test "send_when_update" do
+  #   mail = NoticeMailer.send_when_update
+  #   assert_equal "Send when update", mail.subject
+  #   assert_equal ["to@example.org"], mail.to
+  #   assert_equal ["from@example.com"], mail.from
+  #   assert_match "Hi", mail.body.encoded
+  # end
 
 end
