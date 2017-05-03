8a0c7a8f7adce43c15e392da3e3d2a6f4dfed820
yamada <yshunske@gmail.com>
2017-05-03 13:42:53 +0900

    コントローラ周りのspecはこれから実装のためコメントアウト

--- spec/controllers/off_meetings_controller_spec.rb
+++ spec/controllers/off_meetings_controller_spec.rb
@@ -2,24 +2,20 @@ require "rails_helper"
 
 describe OffMeetingsController do
 
-  describe 'GET #index' do
-    context 'without login' do
-      it 'populates an array of all contacts"' do
-        meet_1 = create(:off_meeting)
-        meet_2 = create(:off_meeting, summary: "テストオフ会2")
-        get :index
-        expect(assigns[:off_meetings]).to match_array([meet_1, meet_2])
-      end
-      it 'renders the :index template' do
-        get :index
-        expect(response).to render_template :index
-      end
-    end
-
-
-
-
-  end
+  # describe 'GET #index' do
+  #   context 'without login' do
+  #     it 'populates an array of all contacts"' do
+  #       meet_1 = create(:off_meeting)
+  #       meet_2 = create(:off_meeting, summary: "テストオフ会2")
+  #       get :index
+  #       expect(assigns[:off_meetings]).to match_array([meet_1, meet_2])
+  #     end
+  #     it 'renders the :index template' do
+  #       get :index
+  #       expect(response).to render_template :index
+  #     end
+  #   end
+  # end
 
 
 end
